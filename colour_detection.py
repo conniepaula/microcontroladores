0c7164eae677550364db9813e1c9c1a76b0cfcd8
Connie <conniedpaula@gmail.com>
2022-11-10 14:30:33 -0300

    Add contour

--- colour_detection.py
+++ colour_detection.py
@@ -8,23 +8,38 @@ while True:
     width = int(cap.get(3))
     height = int(cap.get(4))
 
-    # convert colours to HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+      # convert colours to HSV
+    hsv = cv2.cvtColor(imagem, cv2.COLOR_BGR2HSV)
     # we now pick a lower and an upper bound to the colours that we want to extract from the image
-    lower_blue = np.array([90, 50, 50])
-    upper_blue = np.array([130, 255, 255])
-    # Mask returns part of an image that only has the blue pixels existing
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    lower_orange = np.array([0, 60, 60])
+    upper_orange = np.array([20, 255, 255])
+    # Mask returns part of an image that only has the orange pixels existing
+    mask = cv2.inRange(hsv, lower_orange, upper_orange)
     # Compare mask and image pixel by pixel and turn all pixels that are not blue to black
-    result = cv2.bitwise_and(frame, frame, mask=mask)
-   
-    cv2.imshow('frame', result)
+    result = cv2.bitwise_and(imagem, imagem, mask=mask)
+    # cv2.imshow('imagem', result)
+    
+    contornos,_ = findContours(mask, RETR_TREE, CHAIN_APPROX_SIMPLE)
+    maior_x = 0
+    maior_y = 0
+    maior_comp = 0
+    maior_altura = 0
+    area = 0
+    for contorno in contornos:
+        x, y, comprimento, altura = boundingRect(contorno)
+        if comprimento*altura > area:
+            area = comprimento*altura
+            maior_x = x
+            maior_y = y
+            maior_comp = comprimento
+            maior_altura = altura
+             
+        #rectangle(result, pt1=(x,y), pt2=(x + comprimento, y + altura), color=(0,255,0), thickness=3)
+    if area > 2000:
+        rectangle(imagem, pt1=(maior_x,maior_y), pt2=(maior_x + maior_comp,maior_y + maior_altura), color=(0,255,0), thickness=3)
+    cv2.imshow('imagem', imagem)
 
-    contours,_ = findContours(mask, RETR_TREE, CHAIN_APPROX_SIMPLE)
-    for contour in contours:
-        x,y, width, height = boundingRect(contour)
-        
-    if cv2.waitKey(1) == ord('q'):
+    if waitKey(1) & 0xFF == ord("q"):
         break
 
 cap.release()
